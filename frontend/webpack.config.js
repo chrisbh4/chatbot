b021dc3915975f184aaa1b9989e9973954d21225
Christian Brown <christian@fly.io>
2024-09-24 22:28:29 -0700

    frontend UI app configured

---
+++ frontend/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Ensure 'static' is used instead of 'contentBase'
+    },
+    port: 9000, // Port for the dev server
+    open: true, // Open browser automatically
+    hot: true, // Enable hot module replacement
+  },
+};
+
