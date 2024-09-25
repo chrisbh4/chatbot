7f72e89dabd49a99a8496764c64fa34bab9622eb
Christian Brown <christian@fly.io>
2024-09-25 01:50:10 -0700

    .env added and adjusted CORS secruity

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -5,16 +6,15 @@ from pydantic import BaseModel
 import openai
 import os
 
+load_dotenv()
 # Initialize the FastAPI app
 app = FastAPI()
 
-# Configure OpenAI API Key
-# openai.api_key = os.getenv("OPENAI_API_KEY")  # Set your OpenAI API key here
-openai.api_key = "sk-J7QB2qEH8Ixd0PkKYBQ6uuHbziS_z3zoKQAHWk08JsT3BlbkFJ6varvG4GrSa1J0Dsb_ymU_nh47Z8cWQkGcM6HtnB0A"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Adjust for security, e.g., ['http://localhost:3000']
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -28,19 +28,15 @@ async def get_index():
         content = file.read()
     return content
 
-
 # Model for incoming POST requests
 class MessageRequest(BaseModel):
     message: str
-
 
 # Route to handle POST requests to '/api' and call OpenAI API
 @app.post("/api")
 async def send_message(request: MessageRequest):
     user_message = request.message
 
-    # try:
-        # Send a request to OpenAI's API (e.g., GPT-3.5 or GPT-4)
     response = openai.chat.completions.create(
                 model="gpt-4o-mini",
                 messages=[
@@ -53,14 +49,9 @@ async def send_message(request: MessageRequest):
         print(response.choices[0].message)
         ai_response = response.choices[0].message.content
 
-    # ai_response = response.choices[0].message
         return {"response": ai_response}
     else:
         return "Failed to Generate Response"
 
     # except Exception as e:
     #     return {"error": str(e)}
-
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
