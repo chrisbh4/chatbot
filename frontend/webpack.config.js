4ee6593f8841971acd5160abff68efea99b24699
Christian Brown <christian@fly.io>
2024-09-24 22:39:59 -0700

    basic ui added

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './index.tsx',
+  entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
